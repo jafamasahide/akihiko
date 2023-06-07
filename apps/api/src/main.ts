1df2cfbe4f2cac611a445f0b5fe4543f65b381ae
Jafa Masahide <jafamasahide@gmail.com>
2023-06-08 05:20:14 +0900

    chore(api): add validation pipe as global

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { ApiModule } from './api.module';
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify'
+import { ApiModule } from './api.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(ApiModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    ApiModule,
+    new FastifyAdapter(),
+  )
+  app.useGlobalPipes(new ValidationPipe())
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
